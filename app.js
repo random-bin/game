90e556694a2ca5425c493d186281238ce910d91e
Artur Gurgul <artur.gurgul@gmail.com>
2022-06-13 14:24:59 +0200

    Not to use string directly

--- app.js
+++ app.js
@@ -1,72 +1,110 @@
+Array.prototype.random = function () {
+    return this[Math.floor((Math.random()*this.length))]
+}
 
-class Game {
+class Result {
+    static DRAW = new Result(0)
+    static WIN = new Result(1)
+    static LOSE = new Result(-1)
 
-    constructor() { 
-        this.choiceArr = ["PAPIER", "NOŻYCE", "KAMIEŃ"]
-    }
-    
-    checkWhoWon(userChoice, computerChoice) {
-        if (userChoice == computerChoice) {
-           return 0
+    constructor(val) {
+        this.val = val
+
+        switch (val) {
+            case 0: this.displayString = "Remis"; break
+            case 1: this.displayString = "Wygrałeś"; break
+            case -1: this.displayString = "Przegrałeś"; break
         }
-    
-        if (userChoice == "KAMIEŃ" & computerChoice == "PAPIER") {
-            return -1
-        } 
-        if (userChoice == "NOŻYCE" & computerChoice == "KAMIEŃ") {
-            return -1
-        } 
-        if (userChoice == "PAPIER" & computerChoice == "NOŻYCE") {
-            return -1
-        } 
-        if (userChoice == "KAMIEŃ" & computerChoice == "NOŻYCE") {
-            return 1
-        } 
-        if (userChoice == "NOŻYCE" & computerChoice == "PAPIER") {
-            return 1
-        } 
-        if (userChoice == "PAPIER" & computerChoice == "KAMIEŃ") {
-            return 1
-        }
-    }
-
-    getRandomIntInclusive(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    play() {
-        this.computerChoice = this.choiceArr[this.getRandomIntInclusive(0, 2)]
     }
 }
 
-function play(value){
+class Choice {
+    static STONE = "STONE"
+    static PAPPER = "PAPPER"
+    static SCISSOR = "SCISSOR"
+    
+    static allValues = {}
 
+    static choice(val) {
+        if (Object.keys(Choice.allValues).length == 0) {
+            const stone = new Choice(Choice.STONE)
+            stone.displayString = "Kamień"
+            Choice.allValues[Choice.STONE] = stone
 
-    const game = new Game()
+            const papper = new Choice(Choice.PAPPER)
+            papper.displayString = "Papier"
+            Choice.allValues[Choice.PAPPER] = papper
 
-    game.play()
+            const scissor = new Choice(Choice.SCISSOR)
+            scissor.displayString = "Nożyce"
+            Choice.allValues[Choice.SCISSOR] = scissor
 
-    document.getElementById("compChoicePar").innerHTML = game.computerChoice;
-
-    // User choice
-
-    let userChoice = game.choiceArr[value];
-    document.getElementById("userChoicePar").innerHTML = userChoice;
-
-    // Victory logic
-
-    switch (game.checkWhoWon(userChoice, game.computerChoice)) {
-        case 0:
-            document.getElementById("result").innerHTML = "REMIS"
-            break
-        case 1:
-            document.getElementById("result").innerHTML = "WYGRAŁEŚ"
-            break
-        case 2:
-            document.getElementById("result").innerHTML = "PRZEGRAŁEŚ"
-            break
+            stone.winsWith = scissor
+            scissor.winsWith = papper
+            papper.winsWith = stone
+        }
+        return this.allValues[val]
     }
 
+    static random() {
+        return Object.values(this.allValues).random()
+    }
+
+    constructor(value) {
+        this.value = value
+        this.winsWith = null
+        this.displayString = ""
+    }
+
+
+    wins(other) {
+        if (this === other) {
+            return Result.DRAW
+        }
+        return this.winsWith == other ? Result.WIN : Result.LOSE
+    }
 } 
+
+class Game {
+
+    constructor() {
+        this.computerChoice = null
+        this.userChoice = null
+    }
+    
+    play(userChoice) {
+        this.computerChoice = Choice.random()
+        this.userChoice = userChoice
+    }
+
+    get result() {
+        return this.userChoice.wins(this.computerChoice)
+    }
+}
+
+function play(userChoice){
+
+    const game = new Game()
+    game.play(userChoice)
+
+    document.getElementById("compChoicePar").innerHTML = game.computerChoice.displayString;
+    document.getElementById("userChoicePar").innerHTML = game.userChoice.displayString;
+    document.getElementById("result").innerHTML = game.result.displayString
+} 
+
+window.onload = function() {
+    document.getElementById("STONE").onclick = function() {
+        const userChoice = Choice.choice(Choice.STONE)
+        play(userChoice)
+    }
+
+    document.getElementById("PAPPER").onclick = function() {
+        const userChoice = Choice.choice(Choice.PAPPER)
+        play(userChoice)
+    }
+
+    document.getElementById("SCISSOR").onclick = function() {
+        const userChoice = Choice.choice(Choice.SCISSOR)
+        play(userChoice)
+    }
+}
